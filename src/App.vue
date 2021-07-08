855bd1e473d34be4a1e05cf90997390134c59de9
EgijaLocmele <locmele.egija@gmail.com>
2021-07-08 13:43:54 +0300

    Vue app commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <tabs :data="data"/>
+</template>
+
+<script>
+import data from "./data/data.json";
+import Tabs from "./components/Tabs.vue";
+
+export default {
+  name: "App",
+  components: {
+    Tabs
+  },
+  data() {
+    return {
+      data
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+  cursor: default;
+  overflow-x: hidden;
+  background-color: $darkRed;
+  text-align: center;
+}
+</style>
